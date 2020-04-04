dd976d1f7fd5f15c1f91c905ed154379bbf606d5
ThanhcongPhung <20160500@student.hust.edu.vn>
2020-04-04 20:50:30 +0700

    done comment movie

--- src/components/BoxCommentComponent.js
+++ src/components/BoxCommentComponent.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 
 class BoxCommentComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: ""
+    };
+  }
+  handleSubmit = event => {
+    this.props.onAddToDo(this.state);
+    this.setState({ comment: "" });
+  };
+  handleChangeComment = event => {
+    this.setState({ comment: event.target.value });
+  };
+  
   render() {
     return (
       <div className="row">
@@ -10,9 +24,11 @@ class BoxCommentComponent extends Component {
               <textarea
                 placeholder="コメントする..."
                 className="form-control"
+                value={this.state.comment}
+                onChange={this.handleChangeComment}
               ></textarea>
               <form class="form-inline mt-3">
-                <button class="btn btn-primary pull-right" type="button">
+                <button class="btn btn-primary pull-right" type="button" onClick={this.handleSubmit}>
                   送信
                 </button>
               </form>
